eb9c6db1a88b632621a362c5539538cf838c8642
Sarah Rabah <sarahmrabah@gmail.com>
2019-09-03 13:46:12 +0200

    Adding release description and test images

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import './index.css';
 import logo from '../../assets/logo.gif';
-import TG040 from '../../assets/TG040.jpg'
-import { release } from 'os';
-
+import TG040 from '../../assets/albumArt/TG040.jpg';
+import TG041 from '../../assets/albumArt/TG041.jpg';
+import TG042 from '../../assets/albumArt/TG042.jpg';
+import TG043 from '../../assets/albumArt/TG043.jpg';
 
 const releases = [
   {name: 'Self-Titled', releaseNumber: 'TG040', artist: 'Frail Hands', id:1, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
-  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveyLowLow', id:2, img:TG040},
+  {name: 'Courtside Seats', releaseNumber: 'TG043', artist: 'Heavy Heavy Low Low', id:2, img:TG043},
+  {name: 'Hourglass', releaseNumber: 'TG042', artist: 'Ghost Spirit', id:2, img:TG042},
+  {name: '2020', releaseNumber: 'TG041', artist: 'Shin Guard', id:2, img:TG041},
+  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeavyLowLow', id:2, img:TG043},
+  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeavyLowLow', id:2, img:TG043},
+  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeavyLowLow', id:2, img:TG043},
+  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeavyLowLow', id:2, img:TG043},
+  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeavyLowLow', id:2, img:TG043},
+  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeaveLowLow', id:2, img:TG043},
+  {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeavyLowLow', id:2, img:TG043},
 ]
 
 // class Release extends Component {
+//   state = {
+//     item: ''
+//   }
+
 //   render() {
-//     return (
-//       <div className="release" id={release.releaseNumber}>
-//       </div>
-//     )
+    
+//     <div className="release">
+
+//     </div>
 //   }
 // }
 
@@ -33,7 +38,15 @@ class ReleaseDisplay extends Component {
     return (
       <div className="releaseDisplay">
         { releases.map(release => {
-            return <img src={release.img} className="release" key={release.releaseNumber}/>
+            return (
+              <div className="release">
+                <img src={release.img} className="cover" key={release.releaseNumber} alt={release.name}/>
+                <div className="description">
+                  <div>{release.name}</div>
+                  <div>by {release.artist}</div>
+                </div>
+              </div>
+            )
         })}
       </div>
     )
