3dd7183959fdcdc8e1fba50fbc39883f8963b65c
Sarah Rabah <sarahmrabah@gmail.com>
2019-09-04 11:40:01 +0200

    Adding MouseOver tracking - Currently only console logging release number - Functionality will be built out to add/remove data with hover

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -20,39 +20,6 @@ const releases = [
   {name: 'Courtside Seats', releaseNumber: 'TG012', artist: 'HeavyHeavyLowLow', id:2, img:TG043},
 ]
 
-// class Release extends Component {
-//   state = {
-//     item: ''
-//   }
-
-//   render() {
-    
-//     <div className="release">
-
-//     </div>
-//   }
-// }
-
-class ReleaseDisplay extends Component {
-  render() {
-    return (
-      <div className="releaseDisplay">
-        { releases.map(release => {
-            return (
-              <div className="release">
-                <img src={release.img} className="cover" key={release.releaseNumber} alt={release.name}/>
-                <div className="description">
-                  <div>{release.name}</div>
-                  <div>by {release.artist}</div>
-                </div>
-              </div>
-            )
-        })}
-      </div>
-    )
-  }
-}
-
 class Header extends Component {
 
   render() {
@@ -60,23 +27,102 @@ class Header extends Component {
       <div className="header">
           <img src={logo} alt="Logo" id="logo"/>
           <div id="navLinks">
-            <a id="news">NEWS</a>
-            <a id="about">ABOUT</a>
-            <a id="store">STORE</a>
+            <div id="news">NEWS</div>
+            <div id="news">RELEASES</div>
+            <div id="news">ARTISTS</div>
+            <div id="news">AUDIO</div>
+            <div id="about">ABOUT</div>
+            <div id="store">STORE</div>
           </div>
       </div>
     );
   }
 }
 
-class App extends Component {
 
+class News extends Component {
+
+  render() {
+    return (
+      <div className="news">
+        <div className="sectionHeader">News</div>
+        <div className="freeText">newsnewsnewsnewsnewsnewsnewsnews</div>
+      </div>
+    )
+  }
+}
+
+
+// const todoItem = props => {
+//   return (
+//       <li className={props.todo.completed ? 'TodoItem-completed' : ''}
+//           onClick={() => props.markCompleted(props.todo.id)}>
+//           {
+//               props.todo.item
+//           }
+//       </li>
+//   )
+// }
+
+// export default todoItem
+const Release = props => {
+  return (
+    <div className="release" 
+      onMouseOver={props.onMouseOver} 
+      id={props.release.releaseNumber}
+    >
+        <img src={props.release.img} className="cover" alt= {props.release.name}/>
+        <div className="description">
+          <div>{props.release.name}</div>
+          <div>by {props.release.artist}</div>
+        </div>
+    </div>  
+  )
+}
+
+
+
+class Releases extends Component {
+  state = {
+    item: ''
+  }
+
+  // function detecting mouse movements
+  hoverHandler = event => {
+    console.log(event.currentTarget.id)
+    this.setState({
+      item: event.currentTarget.releaseNumber,
+      active: true,
+    })
+  }
+
+  render() {
+    return (
+      <div id="Releases">
+        <div className="sectionHeader">Releases</div>
+        <div className="releaseDisplay">
+          { releases.map((release, index, hoverHandler) => {
+            return (
+              <Release release={release}  key={index} onMouseOver={this.hoverHandler}/>
+            )
+          })}
+        </div>
+      </div>
+      
+    )
+  }
+}
+
+
+class App extends Component {
 
   render () {
     return (
       <div className="app">
-        <div className="header"><Header /></div>
-        <div className="releaseGrid"><ReleaseDisplay/></div>
+        <Header />
+        <News />
+        <Releases/>
+
       </div>
     )
   }
