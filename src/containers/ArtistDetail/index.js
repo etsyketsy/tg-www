e1a9121ca5705c435fbaa23e6b91d584332410b1
Sarah Rabah <sarahmrabah@gmail.com>
2019-09-13 11:35:44 +0100

    Adding overflow settings and limits to release and artist listings

---
+++ src/containers/ArtistDetail/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class ArtistInfo extends Component {
+    
+    clickOutHandler = () =>{
+        return (
+            pass
+        )
+    }
+
+    render() {
+        return (
+            <div className="artistInfo">
+                details here...
+            </div>
+        )
+    }
+}
+
+export default ArtistInfo;
