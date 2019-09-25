68e7993ce4477287642308af1754c1fa52d3ff71
Sarah Rabah <sarahmrabah@gmail.com>
2019-09-25 18:53:30 +0200

    WIP: Working on connecting Frontend to API - Starting with Artist

--- src/containers/Artists/index.js
+++ src/containers/Artists/index.js
@@ -5,30 +5,30 @@ import releaseData from '../Releases/releaseData';
 class Artist extends Component {
   state = {
     showInfo: false
-}
-clickHandler = event => {
-  this.setState({
-    showInfo: !this.state.showInfo
-  })
-}
+  }
+  clickHandler = event => {
+    this.setState({
+      showInfo: !this.state.showInfo
+    })
+  }
 
   render() {
     return (
       (this.state.showInfo) ?
 
-      <div className="artist"
-        onClick={this.clickHandler}
-        id={this.props.artist.artist}
-      >
+        <div className="artist"
+          onClick={this.clickHandler}
+          id={this.props.artist.artist}
+        >
           <div>Picture coming</div>
-      </div>
-    :
-    <div className="artist"
-        onClick={this.clickHandler}
-        id={this.props.artist.artist}
-      >
+        </div>
+        :
+        <div className="artist"
+          onClick={this.clickHandler}
+          id={this.props.artist.artist}
+        >
           <div>{this.props.artist.artist}</div>
-      </div>
+        </div>
     )
   }
 
@@ -46,32 +46,45 @@ class Artists extends Component {
     })
   }
 
+  componentDidMount() {
+    fetch('http://localhost:8000/backend/api/artist/')
+      .then(response => response.json())
+      .then(data => {
+        this.setState(
+          { artists: data }
+        )
+      })
+  }
+
   render() {
     return (
-      <div className="content" id="artists">
-        <div className="sectionHeader">Artists</div>
-        <div className="displayGrid">
+      this.state.artists.length ?
+        <div className="content" id="artists">
+          <div className="sectionHeader">Artists</div>
+          <div className="displayGrid">
+            {
+              this.state.artists.slice(0, this.state.artistsToRender).map(
+                (artist, index) => {
+                  return (
+                    <Artist artist={artist} key={index} />
+                  )
+                }
+              )
+            }
+          </div>
           {
-            releaseData.slice(0, this.state.artistsToRender).map(
-              (artist, index) => {
-                return (
-                  <Artist artist={artist} key={index}/>
-                )
-              }
-            )
+            (releaseData.length > this.state.artistsToRender) ?
+              <button className="loadMore"
+                id="loadArtists"
+                onClick={this.loadMoreHandler}
+              >
+                Load more...
+              </button>
+              : null
           }
-        </div>
-        {
-          (releaseData.length > this.state.artistsToRender) ?
-            <button className="loadMore" 
-              id="loadArtists" 
-              onClick={this.loadMoreHandler}
-            >
-              Load more...
-            </button>
-            : null
-        }
-      </div>
+          </div>
+        :
+        <h1>loading...</h1>
 
     )
   }
