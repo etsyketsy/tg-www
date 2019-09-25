48dd96632bcf6117773007506227c776cdfbee3b
Sarah Rabah <sarahmrabah@gmail.com>
2019-09-25 18:53:30 +0200

    Merging fetch functionality for Artist page

--- src/containers/Artists/index.js
+++ src/containers/Artists/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import releaseData from '../Releases/releaseData';
 
 
+<<<<<<< HEAD
 const Artist = props => {
   return (
     <div className="artist"
@@ -13,6 +14,38 @@ const Artist = props => {
       </div>
     </div>
   )
+=======
+class Artist extends Component {
+  state = {
+    showInfo: false
+  }
+  clickHandler = event => {
+    this.setState({
+      showInfo: !this.state.showInfo
+    })
+  }
+
+  render() {
+    return (
+      (this.state.showInfo) ?
+
+        <div className="artist"
+          onClick={this.clickHandler}
+          id={this.props.artist.artist}
+        >
+          <div>Picture coming</div>
+        </div>
+        :
+        <div className="artist"
+          onClick={this.clickHandler}
+          id={this.props.artist.artist}
+        >
+          <div>{this.props.artist.artist}</div>
+        </div>
+    )
+  }
+
+>>>>>>> 68e7993... WIP: Working on connecting Frontend to API
 }
 
 class Artists extends Component {
@@ -36,13 +69,37 @@ class Artists extends Component {
     })
   }
 
+<<<<<<< HEAD
+=======
+  componentDidMount() {
+    fetch('http://localhost:8000/backend/api/artist/')
+      .then(response => response.json())
+      .then(data => {
+        this.setState(
+          { artists: data }
+        )
+      })
+  }
+>>>>>>> 68e7993... WIP: Working on connecting Frontend to API
 
   render() {
     return (
-      <div className="content" id="artists">
-        <div className="sectionHeader">Artists</div>
-        <div className="displayGrid">
+      this.state.artists.length ?
+        <div className="content" id="artists">
+          <div className="sectionHeader">Artists</div>
+          <div className="displayGrid">
+            {
+              this.state.artists.slice(0, this.state.artistsToRender).map(
+                (artist, index) => {
+                  return (
+                    <Artist artist={artist} key={index} />
+                  )
+                }
+              )
+            }
+          </div>
           {
+<<<<<<< HEAD
             releaseData.slice(0, this.state.artistsToRender).map((artist, index, hoverHandler) => {
               return (
                 <Artist artist={artist} key={index} onMouseOver={this.hoverHandler} />
@@ -56,6 +113,20 @@ class Artists extends Component {
             : null
         }
       </div>
+=======
+            (releaseData.length > this.state.artistsToRender) ?
+              <button className="loadMore"
+                id="loadArtists"
+                onClick={this.loadMoreHandler}
+              >
+                Load more...
+              </button>
+              : null
+          }
+          </div>
+        :
+        <h1>loading...</h1>
+>>>>>>> 68e7993... WIP: Working on connecting Frontend to API
 
     )
   }
