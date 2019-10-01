38ea370dab4ae28aeb30f673942ce15eddeda4c4
Sarah Rabah <sarahmrabah@gmail.com>
2019-10-01 13:24:26 +0200

    Changing show info methods on release components - Switching from onClick to on MouseEnter for showing basic info - Clicking on item will send user to full detail view

--- src/containers/Release/index.js
+++ src/containers/Release/index.js
@@ -4,21 +4,27 @@ import DetailView from '../DetailView'
 
 class Release extends Component {
     state = {
-        showInfoState: false
+        showInfo: false
     }
 
     clickHandler = (e) => {
-
         this.props.history.push('/release_detail')
     }
 
-    render() {
+    hoverHandler = () => {
+        this.setState(
+            {showInfo: !this.state.showInfo}
+        )
+    }
 
+    render() {
+    
         return (
             (this.state.showInfo) ?
                 <div className="release"
                     id={this.props.item.releaseNumber}
                     onClick={this.clickHandler}
+                    onMouseEnter={this.hoverHandler}
                 >
                     <div>{this.props.item.name}</div>
                     <div>{this.props.item.artist}</div>
@@ -29,6 +35,7 @@ class Release extends Component {
                 <div className="release"
                     id={this.props.item.releaseNumber}
                     onClick={this.clickHandler}
+                    onMouseEnter={this.hoverHandler}
                 >
                     <img src={this.props.item.img}
                         className="cover"
