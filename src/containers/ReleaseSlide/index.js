4ddfa0ace4d14e54d8fb4403c5cfb8d1e0dfd0c7
Sarah Rabah <sarahmrabah@gmail.com>
2019-10-30 20:03:42 +0100

    Cleaning up comments

--- src/containers/ReleaseSlide/index.js
+++ src/containers/ReleaseSlide/index.js
@@ -12,47 +12,44 @@ class ReleaseSlide extends Component {
 
         (!this.props.location.state) ?
             this.props.history.push('/releases')
-        :
+            :
             this.props.history.goBack()
     }
 
     componentDidMount() {
         (!this.props.location.state) ?
-        
-        fetch(`http://localhost:8000/backend/api/release/${this.props.match.params.cat_num}/`)
-        .then(response => response.json())
-        .then(data => {
-            this.setState(
-                { item: data[0] }
-            )
-        })
-        :
-        this.setState({
-            item: this.props.location.state.release
-        })
+
+            fetch(`http://localhost:8000/backend/api/release/${this.props.match.params.cat_num}/`)
+                .then(response => response.json())
+                .then(data => {
+                    this.setState(
+                        { item: data[0] }
+                    )
+                })
+            :
+            this.setState({
+                item: this.props.location.state.release
+            })
     }
 
     render() {
-        console.log('in release detail')
-        console.log(this.props)
         return (
             (!this.state.item) ?
-            <h2>loading...</h2>
-            :
-            
-            <div className="releaseSlide" id={this.props.index}>
-                {/* <Link to="/releases" replace id="exit">&#215;</Link> */}
-                <button onClick={this.clickHandler}>&#215;</button>
-                <div className="desc">
-                    <h2>{this.state.item.release_title}</h2>
-                    {this.state.item.fk_artist}
-                    {this.state.item.tracklisting}
-                    <br></br>
-                    {this.state.item.cat_num}
-                    <br></br> 
+                <h2>loading...</h2>
+                :
+
+                <div className="releaseSlide" id={this.props.index}>
+                    <button onClick={this.clickHandler}>&#215;</button>
+                    <div className="desc">
+                        <h2>{this.state.item.release_title}</h2>
+                        {this.state.item.fk_artist}
+                        {this.state.item.tracklisting}
+                        <br></br>
+                        {this.state.item.cat_num}
+                        <br></br>
+                    </div>
+
                 </div>
-                
-            </div>
 
 
         )
