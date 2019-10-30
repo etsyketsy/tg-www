c0af356907d944447e617feb55867238a27df991
Sarah Rabah <sarahmrabah@gmail.com>
2019-10-30 19:33:51 +0100

    Fixing ReleasePreview to pull from API and render only when on Home page

--- src/containers/Releases/ReleasesPreview.js
+++ src/containers/Releases/ReleasesPreview.js
@@ -1,39 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import releaseData from './releaseData.js';
 import Release from '../Release';
 
 
-class ReleasePreview extends Component {
+class ReleasesPreview extends Component {
 
     state = {
         itemsToRender: 4,
     }
 
-
-    viewMoreHandler = () => {
-        this.props.history.push('/releases');
-
-    }
-
-    onComponentDidMount() {
-        fetch('http://localhost:8000/backend/api/release/')
-        .then(response => response.json())
-        .then(data => {
-          this.setState(
-            { releases: data }
-          )
-        })
-    }
+    // onComponentDidMount() {
+    //     fetch('http://localhost:8000/backend/api/release/')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       this.setState(
+    //         { releases: data }
+    //       )
+    //     })
+    // }
 
 
     render() {
         return (
+
             <div className="content" id="releasePreview">
                 <div className="sectionHeader">Releases</div>
                 <div className="displayGrid">
                     {
-                        releaseData.slice(0, this.state.itemsToRender).map(
+                        this.props.releases.slice(0, this.state.itemsToRender).map(
                             (release, index) => {
                                 return (
                                     <Release
@@ -52,4 +46,4 @@ class ReleasePreview extends Component {
     }
 }
 
-export default ReleasePreview;
+export default ReleasesPreview;
