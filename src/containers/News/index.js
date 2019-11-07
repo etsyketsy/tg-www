befe67dada6867759fc0c620db3a07c5969b0bc5
Sarah Rabah <sarahmrabah@gmail.com>
2019-11-07 18:36:36 +0100

    Fixing CORS issue with RSS call - Also removed HTML React Parser package

--- src/containers/News/index.js
+++ src/containers/News/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import RSSParser from 'rss-parser';
-import { stringLiteral } from '@babel/types';
 
 
 class News extends Component {
@@ -21,51 +20,60 @@ class News extends Component {
     //         let dataArray = Array.from(elements)
     //         console.log(dataArray)
     //         this.setState({news: dataArray}) 
-            
+
     //     })
     // }
 
-    // receives XML doc, but can't parse it
+    //receives XML doc, but can't parse it
     // componentDidMount() {
     //     let request = new XMLHttpRequest();
 
     //     request.open('GET', 'https://blog.tgrex.com/rss');
     //     request.send();
     //     request.onload = () => {
-    //         console.log(request.response)
+    //         // console.log(request.responseText)
     //         let data = JSON.parse(request.responseText)
     //         console.log(data)
     //     }
     // }
 
+    parsePost = (content) => {
+        let wrapper = document.createElement("textarea");
+        wrapper.innerHTML = content;
+        console.log(wrapper)
+        return wrapper.textContent
+    }
+
+    // *** best so far
     componentDidMount() {
         let parser = new RSSParser();
-        parser.parseURL('https://blog.tgrex.com/rss')
-        .then(feed => {
-            this.setState({ posts: feed.items})
-        })
-
+        parser.parseURL('https://cors-anywhere.herokuapp.com/https://blog.tgrex.com/rss')
+            .then(feed => {
+                this.setState({ posts: feed.items })
+            })
     }
 
     render() {
         return (
-           <div id='news'>
+            <div id='news'>
                 <div>'coming soon'</div>
                 {
                     (!this.state.posts) ?
                         <div>'coming soon'</div>
-                    :
-                        
+                        :
+
                         this.state.posts.map((post, index) => {
-                            console.log(post.content)
+               
+                
                             return (
                                 <div className='post' key={index}>
-                                    <h4>{post.title}</h4>
-                                    <p>{post.contentSnippet}</p>
-                                    <a href={post.link}>view post</a>
+                                    {/* <h4>{post.title}</h4>
+                                    
+                                    <a href={post.link}>view post</a> */}
+                                    {post.content}
                                 </div>
                             )
-                            
+
                         })
                 }
             </div>
