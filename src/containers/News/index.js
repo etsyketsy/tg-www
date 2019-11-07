826143c033c2d45e1a9c83553e2f45871b57cd70
Sarah Rabah <sarahmrabah@gmail.com>
2019-11-07 15:11:08 +0100

    Adding rss-parser package and code to parse XML to json - Posts are converted to objects and return most content - Main post content is still encoded in html so not rendering correctly

--- src/containers/News/index.js
+++ src/containers/News/index.js
@@ -1,48 +1,71 @@
 import React, { Component } from 'react';
+import RSSParser from 'rss-parser';
+import { stringLiteral } from '@babel/types';
 
 
 class News extends Component {
     state = {
-        news: ''
+        posts: ''
     }
 
+    // // shows html, but processed as string, not DOM elements
     // componentDidMount() {
+    //     let news = document.getElementById( "news" );
     //     fetch('https://blog.tgrex.com/rss', {
     //         method: 'GET',
     //     })
     //     .then(response => response.text())
     //     .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-    //     .then(data => this.setState({ news: data})
-    //     ); 
+    //     .then(data => {
+    //         let elements = data.getElementsByTagName('item')
+    //         let dataArray = Array.from(elements)
+    //         console.log(dataArray)
+    //         this.setState({news: dataArray}) 
+            
+    //     })
+    // }
+
+    // receives XML doc, but can't parse it
+    // componentDidMount() {
+    //     let request = new XMLHttpRequest();
+
+    //     request.open('GET', 'https://blog.tgrex.com/rss');
+    //     request.send();
+    //     request.onload = () => {
+    //         console.log(request.response)
+    //         let data = JSON.parse(request.responseText)
+    //         console.log(data)
+    //     }
     // }
 
     componentDidMount() {
-        
-        
-        fetch('https://blog.tgrex.com/rss', {
-            method: 'GET',
+        let parser = new RSSParser();
+        parser.parseURL('https://blog.tgrex.com/rss')
+        .then(feed => {
+            this.setState({ posts: feed.items})
         })
-        .then(response => response.text())
-        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-        .then(data => {
-            let elements = data.getElementsByTagName('item')
-            let dataArray = Array.from(elements)
-            console.log(dataArray)
-            this.setState({news: dataArray}) 
-        })
+
     }
 
     render() {
-        console.log(this.state)
         return (
            <div id='news'>
                 <div>'coming soon'</div>
                 {
-                    (!this.state.news) ?
+                    (!this.state.posts) ?
                         <div>'coming soon'</div>
                     :
-                        this.state.news.map(item => {
-                            return (item.innerHTML)
+                        
+                        this.state.posts.map((post, index) => {
+                            console.log(post.content)
+                            return (
+                                <div className='post' key={index}>
+                                    <h4>{post.title}</h4>
+                                    <p>{post.contentSnippet}</p>
+                                    <a href={post.link}>view post</a>
+                                </div>
+                            )
+                            
                         })
                 }
             </div>
